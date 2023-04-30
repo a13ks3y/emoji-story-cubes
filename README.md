a6b46b30775f58678a9ee587e8e17c08467f0542
Oleksii Smirnov <o.smirnov@e-tender.ua>
2023-04-30 18:58:56 +0300

    fix some emojis not visible (not tested!)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# emoji-story-cubes
-The Story Cubes game, but emojys using for dice-faces instead.
+# Emoji-Story-Cubes
+The Story Cubes game, but emojis using for dice-faces instead.
+Inspired by [story cubes](https://www.storycubes.com/en/games/rorys-story-cubes-classic/)
+
+### AI generation feature
+Just an API call to ChatGPT or similar service.
+### Share feature
+To make share feature working,
+there is two possible solutions:
+1. Generate an image with emojis,
+   and share it like a picture, with a story as
+   description, and emojis in the title.
+2. Generate a URL, with some-way-encoded-hash,
+   which would be used by web-app to render a story.
+   (It could be a completely separate html page,
+   or it could be current application built for
+   the browser platform)
+### Todo:
+* Use React/ReactNative (?)
+* Switch to capacitor (?)
+~~* Fix some emojis not visible on Android (!)~~
+* Fix share button isn't touchable (!)
+* Use cordova-social-share plugin properly
+* Share feature. Choose implementation (implement both?)
+
+
--- www/js/index.js
+++ www/js/index.js
@@ -13,15 +13,25 @@ function generateAllEmojiCodes() {
         [0x261D, 0x261D],
         [0x270A, 0x270D],
         [0x1F300, 0x1F320],
-        [0x1F324, 0x1F4FF],
-        [0x1F500, 0x1F53D],
-        [0x1F549, 0x1F579],
-        [0x1F57A, 0x1F57A],
-        [0x1F590, 0x1F590],
-        [0x1F595, 0x1F596],
-        [0x1f600, 0x1F6D2],
+        //[0x1F324, 0x1F32b], // weather
+        [0x1F32d, 0x1F394],
+        [0x1F3A0, 0x1F3BF],
+        [0x1F3E1, 0x1F3F1],
+        [0x1F400, 0x1F4FF],
+
+
+        [0x1F506, 0x1F518],
+        [0x1F525, 0x1f52F],
+        [0x1F595, 0x1F596], // :)
+
+        [0x1f600, 0x1F636],
+        [0x1f636, 0x1F650],
+        [0x1f680, 0x1F6BF],
         [0x1F910, 0x1F94C],
         [0x1F950, 0x1F9E6],
+        // ???
+        // [0x1F3D0, 0x1F3E0],
+
     ];
 
     for (let i = 0; i < emojiRange.length; i++) {
@@ -35,7 +45,7 @@ function generateAllEmojiCodes() {
 
 const btnShare = document.getElementById('btn-share');
 btnShare.addEventListener('click', () => {
-    if(!navigator['share']) return;
+    if (!navigator['share']) return;
     navigator.share({
         'title': 'The catch-title-share-text, to be done.',
         'text': 'Optional message',
@@ -48,9 +58,10 @@ btnShare.addEventListener('click', () => {
 });
 
 const $qubes = [
-    1,2,3,4,5,6,7,8,9
+    1, 2, 3, 4, 5, 6, 7, 8, 9
 ].map(n => document.getElementById(`qube-${n}`));
 const ROUNDS = 9;
+
 function shuffle() {
     const VALUES = generateAllEmojiCodes();
     for (let i = 1; i <= ROUNDS; i++) {
@@ -62,8 +73,11 @@ function shuffle() {
         }, i * 666);
     }
 }
+
 const tapArea = document.getElementById('tap-area');
 tapArea.addEventListener('click', shuffle);
 tapArea.addEventListener('touchend', shuffle);
 // @todo: check url, and if hash value is set, use it
 shuffle();
+
+// document.body.innerHTML = generateAllEmojiCodes().map(c => `&#${c};`).join('\n');
