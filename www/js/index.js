c8c5e07370b804dc201bf8429599f349ea8ed38b
Oleksii Smirnov <o.smirnov@e-tender.ua>
2023-05-01 13:14:09 +0300

    Basic api call to chat-gpt, to generate the sotry

--- www/js/index.js
+++ www/js/index.js
@@ -80,4 +80,35 @@ tapArea.addEventListener('touchend', shuffle);
 // @todo: check url, and if hash value is set, use it
 shuffle();
 
-// document.body.innerHTML = generateAllEmojiCodes().map(c => `&#${c};`).join('\n');
+// @todo: move to separate file?
+
+async function requestStory() {
+    const emojisStr = $qubes.map($q => $q.textContent).join();
+    const url = 'https://chatgpt-best-price.p.rapidapi.com/v1/chat/completions';
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '800afd36a8mshaefcd630686ba6dp1cab7bjsnd0a31eab74be',
+            'X-RapidAPI-Host': 'chatgpt-best-price.p.rapidapi.com'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: 'Tell a short story,  based on following emojis:' + emojisStr
+                }
+            ]
+        })
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result.choices[0].message.content);
+    } catch (error) {
+        console.error(error);
+    }
+
+}
