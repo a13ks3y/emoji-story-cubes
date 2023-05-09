c6960ca156b7ea7c47bf5ed08ddb2c7e74fbb261
osmirnov <osmirnov@arlo.com>
2023-05-09 11:35:07 +0300

    some refactor, icons for buttons, UI improvements.

---
+++ www/js/requestStory.js
@@ -0,0 +1,28 @@
+async function requestStory(emojisStr) {
+    const url = 'https://chatgpt-best-price.p.rapidapi.com/v1/chat/completions';
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '800afd36a8mshaefcd630686ba6dp1cab7bjsnd0a31eab74be',
+            'X-RapidAPI-Host': 'chatgpt-best-price.p.rapidapi.com'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: 'Tell a short story,  based on following emojis:' + emojisStr
+                }
+            ]
+        })
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result.choices[0].message.content;
+    } catch (error) {
+        console.error(error);
+    }
+}
