7056430eccd0b26f906af3086eb408f8dedaa1c4
Oleksii Smirnov <o.smirnov@e-tender.ua>
2023-06-01 09:42:53 +0300

    🏎️

--- post-build.js
+++ post-build.js
@@ -2,5 +2,10 @@ const fs = require("fs");
 fs.copyFile(
     "./platforms/android/app/build/outputs/apk/debug/app-debug.apk",
     "./emoji-story-cubes_debug.apk",
-    err => err ? console.error(err) : console.log('copied')
+    err => err ? console.error(err) : console.log('.apk copied')
 );
+fs.copyFile(
+    "./platforms/android/app/build/outputs/bundle/release/app-release.aab",
+    "app-release.aab",
+    err => err ? console.error(err) : console.log('.aab copied')
+)
